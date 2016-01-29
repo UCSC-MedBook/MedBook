45194f6cbf520b0ee3dcf86d27e37dfa28fffa27
Mike Risse <rissem@gmail.com>
2016-01-29 09:18:11 -0800

    build off of compose name from medbook rootdir

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-cd $1 && ROOT_URL="http://localhost:8080" docker-compose -p medbook --x-networking -f ../compose-dev.yml build
+ROOT_URL="http://localhost:8080" docker-compose -p medbook --x-networking -f compose-dev.yml build $1
