cb90b7665dd227023b5b7023bdaa70a1840dbde0
Teo Fleming <mokolodi1@gmail.com>
2016-06-22 20:17:47 +0000

    working on dumping mongo to staging

---
+++ scripts/dump_to_staging.sh
@@ -0,0 +1,34 @@
+# create folder and go to it
+dump_dir="~/dump-for-staging"
+if [ ! -d "$dump_dir" ]; then
+    mkdir $dump_dir
+fi;
+cd $dump_dir
+
+# dump the database
+#mongodump -host mongo
+
+# zip the output to send to the staging server
+#tar zcvf dump.zip dump
+
+# delete the unzipped dump
+rm -rf dump
+
+# send it to staging
+staging_ip="13.91.92.109"
+scp dump.zip ubuntu@$staging_ip:/home/ubuntu/
+
+# unzip on staging
+ssh ubuntu@$staging_ip <<-'ENDSSH'
+    cd /home/ubuntu/
+
+    # unzip on staging
+    tar xf dump.zip
+
+    # TODO: move to permenant location and rename with date
+
+    # restore on staging
+    mongorestore -host mongo-staging
+ENDSSH
+
+echo "done!"
