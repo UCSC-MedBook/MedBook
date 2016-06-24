d66cd7c721666a847a6670c7aa2cf412b463b767
Teo Fleming <mokolodi1@gmail.com>
2016-06-24 02:54:05 +0000

    works on staging, fixed GSEA result bug in patient-care

--- startDev.sh
+++ startDev.sh
@@ -1,3 +1,10 @@
 export COMPOSE_HTTP_TIMEOUT=360
-export MONGO_URL="mongodb://mongo:27017/MedBook"
+
+set_up_env="/home/ubuntu/set_up_environment.sh"
+if test -f $set_up_env; then
+    source $set_up_env
+else
+    export MONGO_URL="mongodb://mongo:27017/MedBook"
+fi
+
 docker-compose -f docker-compose.yml -f docker-compose-dev.yml up
