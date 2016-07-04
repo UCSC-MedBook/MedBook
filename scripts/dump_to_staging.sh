4bcc04a896d483b13c0b9d81aa2399b38e096e9c
Teo Fleming <mokolodi1@gmail.com>
2016-07-04 06:07:37 +0000

    fixed dump_to_staging

--- scripts/dump_to_staging.sh
+++ scripts/dump_to_staging.sh
@@ -1,15 +1,15 @@
 # create folder and go to it
-dump_dir="~/dump-for-staging"
+dump_dir="/home/ubuntu/dump-for-staging"
 if [ ! -d "$dump_dir" ]; then
     mkdir $dump_dir
 fi;
 cd $dump_dir
 
 # dump the database
-#mongodump -host mongo
+mongodump -host mongo
 
 # zip the output to send to the staging server
-#tar zcvf dump.zip dump
+tar zcvf dump.zip dump
 
 # delete the unzipped dump
 rm -rf dump
