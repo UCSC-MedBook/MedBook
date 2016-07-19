f90c355aaa52c5d402826ad458117dcde6b1ca0e
Teo Fleming <mokolodi1@gmail.com>
2016-07-19 09:33:10 -0700

    fixed Ellen's notes on collaboration migration script

--- scripts/migrations/new_collaborations_on_users.js
+++ scripts/migrations/new_collaborations_on_users.js
@@ -1,22 +1,21 @@
 // to run:
 // mongo MedBook < new_collaborations_on_users.js
 
+// This is so that we can share things before users with accounts
+// have re-logged in to the new medbook.io. Currently many users have
+// the old collaborations schema (an array), and we don't want to force
+// them all to re-log in to share things with them.
+
 print("Starting new_collaborations_on_users migration");
 
-// Make a copy of the users collection
-// so that this script can be run many times.
-// Then restore from the pre-migration users collection
-if (!db.pre_migration_users.findOne()) {
-  db.users.renameCollection("pre_migration_users");
-  print("renamed old users collection to pre_migration_users");
-}
-
-db.pre_migration_users.copyTo("users");
-print("restored users collection from pre-migration collection");
+// Make a copy of the users collection so that we have a backup
+db.users.copyTo("pre_migration_users");
+print("backed up users collection to pre_migration_users");
 
 // add/update the collaborations field on username/password accounts
 db.users.find({
-  "services.password": { $exists: true }
+  "services.password": { $exists: true },
+  "collaborations.memberOf": { $exists: false },
 }).forEach(function (user) {
   var email = user.emails[0].address;
   print("email:", email);
